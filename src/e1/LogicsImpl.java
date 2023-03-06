a1b04c6033b380ac498e25ea0fe682d8e0385875
Alberto Paganelli <albi1600@gmail.com>
2023-03-06 11:19:52 +0100

    refactor hit() for LogicImpl

--- src/e1/LogicsImpl.java
+++ src/e1/LogicsImpl.java
@@ -29,17 +29,30 @@ public class LogicsImpl implements Logics {
     
 	@Override
 	public boolean hit(int row, int col) {
+		if(canMove(row, col)){
+			move(row, col);
+			return onMove();
+		}
+		return false;
+	}
+
+	private boolean onMove() {
+		return this.pawn.equals(this.knight);
+	}
+
+	private void move(int row, int col) {
+		int x = row - this.knight.getX();
+		int y = col - this.knight.getY();
+		if (x != 0 && y != 0 && Math.abs(x) + Math.abs(y) == 3) {
+			this.knight = new Pair<>(row, col);
+		}
+	}
+
+	private boolean canMove(int row, int col) {
 		if (row<0 || col<0 || row >= this.size || col >= this.size) {
 			throw new IndexOutOfBoundsException();
 		}
-		// Below a compact way to express allowed moves for the knight
-		int x = row-this.knight.getX();
-		int y = col-this.knight.getY();
-		if (x!=0 && y!=0 && Math.abs(x)+Math.abs(y)==3) {
-			this.knight = new Pair<>(row,col);
-			return this.pawn.equals(this.knight);
-		}
-		return false;
+		return true;
 	}
 
 	@Override
